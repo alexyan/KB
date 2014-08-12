e2c0a3dc64a15ca24a8fff125f54b907ae06db5f
想当当 <donghua.yan@alipay.com>
2014-08-13 01:01:04 +0800

    update

---
+++ bin/main.js
@@ -0,0 +1,34 @@
+#!/usr/bin/env node
+
+var commander       = require('commander')
+    , conf          = require('../package.json')
+    , Keyboard      = require('../lib/keyboard');
+
+commander
+    .version(conf.version)
+    .parse(process.argv);
+
+commander
+    .command('on')
+    .description("Enable bluetooth keyboard")
+    .action(function(){
+        Keyboard.on();
+    });
+
+commander
+    .command('off')
+    .description("Disable bluetooth keyboard")
+    .action(function(){
+        Keyboard.off();
+    });
+
+commander.parse(process.argv);
+var args = process.argv.slice(3);
+var subcmd = commander.args[0];
+if (!subcmd) {
+    process.stdout.write(commander.helpInformation());
+    commander.emit('--help');
+    process.exit();
+}
+
+
